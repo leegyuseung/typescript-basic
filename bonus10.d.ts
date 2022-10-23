dd957ef1d9dc84b2ff4ba9101af495beea1a33b4
leegyuseung <lgs4002@naver.com>
2022-10-23 22:02:23 +0900

    d.ts파일이용

---
+++ bonus10.d.ts
@@ -0,0 +1,5 @@
+// 타입정의 보관용 파일입니다.
+export type Agesss = number;
+export interface Personsss {
+  name: string;
+}
---
+++ bonus10.ts
@@ -0,0 +1,5 @@
+// import { Agesss } from "./bonus10.d.ts";
+
+// let ageB: Agesss;
+
+// d.ts파일은 자동 글로벌 모듈이 아니다. > export해야한다.
